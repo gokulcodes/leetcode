e4640ae254476e8e49fe6716da5cc6581723cd5c
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-03-23 13:09:53 +0530

    Time: 0 ms (100.00%), Space: 6 MB (30.21%) - LeetHub

--- 991-broken-calculator/991-broken-calculator.cpp
+++ 991-broken-calculator/991-broken-calculator.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     int brokenCalc(int st, int tr, int cnt = 0) {
-        if(st >= tr){
-            return (st - tr) + cnt;
-        }
+        if(st >= tr) return (st - tr) + cnt;
         
         int l = tr;
         if(tr&1) l++, cnt++;
