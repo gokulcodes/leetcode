c8747e2c8d86213d1673c3aa838c3821361e4bad
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-03-29 06:57:19 +0530

    Time: 164 ms (53.01%), Space: 61.2 MB (91.55%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,11 +1,9 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        map<int, int> m;
-        for(int i = 0; i < nums.size(); i++){
-            m[nums[i]]++;
-            if(m[nums[i]] > 1) return nums[i];
+        while(nums[0] != nums[nums[0]]){
+            swap(nums[0], nums[nums[0]]);
         }
-        return -1;
+        return nums[0];
     }
 };
