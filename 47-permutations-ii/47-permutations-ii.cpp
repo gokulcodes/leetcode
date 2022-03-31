22dd3d3b00c91a601519464fbd8c5d7b39b2582e
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-03-31 06:58:42 +0530

    Time: 15 ms (49.94%), Space: 10 MB (35.67%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<vector<int>> uniqs;
+    
+    void permute(int idx, vector<int>& nums, map<vector<int>, bool>& res){
+        if(idx == nums.size()){
+            if(res[nums]) return;
+            res[nums] = true;
+            uniqs.push_back(nums);
+            return;
+        }
+       
+        if(res[nums]) return;
+        
+        for(int i = 0; i < nums.size(); i++){
+            swap(nums[i], nums[idx]);
+            permute(idx + 1, nums, res);
+            swap(nums[i], nums[idx]);
+        }
+    }
+    
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        map<vector<int>, bool> hash;
+        permute(0, nums, hash);
+        // for(auto x : hash) uniqs.push_back(x);
+        return uniqs;
+    }
+    
+};
