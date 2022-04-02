3b803b5ada8db06e2ffe4bdb48a16b309b0b2cf0
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-04-02 14:49:01 +0530

    Time: 4 ms (27.89%), Space: 6.7 MB (23.11%) - LeetHub

---
+++ 394-decode-string/394-decode-string.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    string decodeString(string s) {
+        stack<string> st;
+        
+        string res = "";
+        
+        for(int i = 0; i < s.length(); i++){
+            if(s[i] != ']'){
+                st.push(string(1, s[i]));
+                continue;
+            }
+            
+            string t = "";
+            while(!st.empty() && st.top() != "["){
+                t = st.top() + t;
+                st.pop();
+            }
+            
+            st.pop();
+            
+            string times = "";
+            while(!st.empty() && st.top() >= "0" && st.top() <= "9"){
+                times = st.top() + times;
+                st.pop();
+            }
+            
+            int cnt = stoi(times);
+            
+            string newS = "", repeat = "";
+            while(!st.empty() && st.top() != "["){
+                newS = st.top() + newS;
+                st.pop();
+            }
+            
+            for(int j = 1; j <= cnt; j++) repeat += t;
+            
+            st.push(newS + repeat);
+            
+            // cout << newS + repeat << endl;
+        }
+        
+        while(!st.empty()){
+            res = st.top() + res;
+            st.pop();
+        }
+        
+        return res;
+    }
+};
+
+// bcbc
