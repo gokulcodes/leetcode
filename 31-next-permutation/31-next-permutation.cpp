766e8185db9c15e665ce6633b6c4062e394106b9
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-04-03 17:52:42 +0530

    Time: 12 ms (13.14%), Space: 11.9 MB (93.15%) - LeetHub

--- 31-next-permutation/31-next-permutation.cpp
+++ 31-next-permutation/31-next-permutation.cpp
@@ -1,9 +1,39 @@
-#include <bits/stdc++.h>
 #include <algorithm>
 
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(), nums.end());
+        int n = nums.size();
+        if(is_sorted(nums.rbegin(), nums.rend())){
+            reverse(nums.begin(), nums.end());
+            return;
+        }
+        
+        int i = 0;
+        for(i = n - 1; i > 0; i--){
+            if(nums[i - 1] < nums[i]) {
+                i = i - 1;
+                break;
+            }
+        }
+        
+        int l = i + 1, r = n - 1, j = i;
+        while(l <= r){
+            int md = (l + r) / 2;
+            
+            if(nums[md] > nums[i]){
+                j = md;
+                l = md + 1;
+            }else r = md - 1;
+        }
+        
+        cout << j << endl;
+        
+        swap(nums[j], nums[i]);
+        
+        for(auto x : nums) cout << x << " ";
+        
+        sort(nums.begin() + i + 1, nums.end());
+        
     }
 };
