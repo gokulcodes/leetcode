468015ac2f56a624360939dd5ef98078918c722a
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-04-14 05:43:29 +0530

    Time: 46 ms (73.83%), Space: 34.8 MB (24.22%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -12,17 +12,14 @@
 class Solution {
 public:
     
-    TreeNode* dfs(TreeNode* root, int val){
+    TreeNode* searchBST(TreeNode* root, int val) {
         if(root == NULL) return NULL;
         
         if(root->val == val) return root;
-        if(root->val > val) 
-            return dfs(root->left, val);
         
-        return dfs(root->right, val);
-    }
-    
-    TreeNode* searchBST(TreeNode* root, int val) {
-        return dfs(root, val);
+        if(root->val > val) 
+            return searchBST(root->left, val);
+        
+        return searchBST(root->right, val);
     }
 };
