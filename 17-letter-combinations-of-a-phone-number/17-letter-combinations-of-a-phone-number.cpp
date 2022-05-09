6ea296ac4ba57083c44651e80d0f3afbfbff9972
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-05-09 14:13:21 +0530

    Time: 4 ms (26.23%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -3,26 +3,15 @@ public:
     
     map<int, string> mp;
     
-    void permute(string s, string t, set<string>& comb, int n, string digit){
-        if(t.size() == n){
-            bool ok = true;
-            for(int i = 0; i < n; i++){
-                if(mp[digit[i] - '0'].find(t[i]) == string::npos) {
-                    ok = false;
-                    break;
-                }
-            }
-            
-            if(ok){
-                comb.insert(t);
-            }
-            
+    void permute(int i, string d, string t, vector<string> &res){
+        if(i >= d.length()){
+            res.push_back(t);
             return;
         }
         
-        for(int i = 0; i < s.length(); i++){
-            t.push_back(s[i]);
-            permute(s, t, comb, n, digit);
+        for(auto x : mp[d[i] - '0']){
+            t += string(1, x);
+            permute(i + 1, d, t, res);
             t.pop_back();
         }
         
@@ -32,20 +21,11 @@ public:
         
         mp[2] = "abc", mp[3] = "def", mp[4] = "ghi", mp[5] = "jkl", mp[6] = "mno", mp[7] = "pqrs", mp[8] = "tuv", mp[9] = "wxyz";
         
-        set<string> comb;
         vector<string> res;
-        
         if(digits.length() == 0) return res;
         
-        string list = "", temp = "";
-        for(auto x : digits){
-            list += mp[x - '0'];
-        }
-        
-        int n = digits.length();
-        
-        permute(list, temp, comb, n, digits);
-        for(auto x : comb) res.push_back(x);
+        string t = "";
+        permute(0, digits, t, res);
         
         return res;
             
