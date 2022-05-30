2510c6c96ee64bf79b841023d018a6a8b149474c
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-05-30 07:57:00 +0530

    Time: 3 ms (67.24%), Space: 5.8 MB (76.09%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,8 +1,20 @@
 class Solution {
 public:
     int divide(long long dividend, long long divisor) {
-        long long r = dividend / divisor;
+        int sign = ((dividend < 0) ^ (divisor < 0)) ? -1 : 1;
         
-        return r > INT_MAX ? INT_MAX : r < INT_MIN ? INT_MIN : int(r);
+        dividend = abs(dividend);
+        divisor = abs(divisor);
+        
+        long long qn = 0, temp = 0;
+        
+        for(int i = 31; i >= 0; i--){
+            if(temp + (divisor << i) <= dividend){
+                temp += (divisor << i);
+                qn |= (1LL << i);
+            }
+        }
+        
+        return (qn * sign) > INT_MAX ? INT_MAX : (qn * sign) < INT_MIN ? INT_MIN : (qn * sign);
     }
 };
