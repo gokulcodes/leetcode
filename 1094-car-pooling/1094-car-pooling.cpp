6ae68df73e51fe154e86965065221e3e20cb4618
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-06-26 17:45:45 +0530

    Time: 40 ms (19.05%), Space: 18.1 MB (5.85%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.cpp
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -7,28 +7,21 @@ public:
         
         int n = trips.size(), people = 0;
         
-        priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int> > > > pq;
+        priority_queue<pair<int, int>, vector<pair<int, int >>, greater<pair<int, int > > > pq;
         
         for(int i = 0; i < n; i++){
             
-            // if(!pq.empty()){
-            //     cout << pq.top().first << " " << pq.top().second.first << " " << pq.top().second.second << endl;
-            // }
-            
             while(!pq.empty() && pq.top().first <= trips[i][1]){
-                people -= pq.top().second.second;
+                people -= pq.top().second;
                 pq.pop();
             }
             
-            // cout << people << endl;
-            
             if(people + trips[i][0] <= capacity){
                 
-                pq.push({ trips[i][2], { trips[i][1], trips[i][0] } });
+                pq.push({ trips[i][2], trips[i][0] });
                 people += trips[i][0];
                 
             }else return false;
-            
             
             
         }
