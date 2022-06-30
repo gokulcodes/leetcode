568551e0f9e066738802bb367b7aebc4d7073b49
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-06-30 07:43:42 +0530

    Time: 17 ms (49.26%), Space: 10.9 MB (15.75%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        
+        long long res = 0, n = nums.size();
+        
+        sort(nums.begin(), nums.end());
+        
+        // for(auto x : nums){
+        //     cout << x << ' ';
+        // }
+        // cout << endl;
+        
+        // cout << nums[n / 2] << endl;
+        
+        int avg = accumulate(nums.begin(), nums.end(), 0) / n;
+        
+        for(auto x : nums) res += max(nums[n / 2], x) - min(nums[n / 2], x);
+        
+        return res;
+    }
+};
+
+
+
+// 1 + 10 + 2 + 9 = 22 / 4 = 5
+// 4 + 5 + 3 + 4 = 16
+    
+// 1 + 2 + 3 = 6 / 3 = 2
+// 1 + 0 + 1 = 2
