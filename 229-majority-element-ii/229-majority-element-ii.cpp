d7dd9e866fe07b5c606b24dc02ac084558b9f3cd
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-07-10 18:04:19 +0530

    Time: 35 ms (6.21%), Space: 15.8 MB (81.73%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.cpp
+++ 229-majority-element-ii/229-majority-element-ii.cpp
@@ -1,20 +1,49 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int n = nums.size();
-        int mr = n / 3;
+        int cnt1 = 0, cnt2 = 0, n = nums.size();
         
-        map<int, int> mp;
+        if(n == 1) return nums;
+        
+        int m1 = nums[0], m2 = INT_MAX;
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] != m1) {
+                m2 = nums[i];
+                break;
+            }
+        }
+        
+        if(m2 == INT_MAX) return {m1};
+        
+        for(int i = 0; i < n; i++){
+            if(nums[i] == m1){
+                cnt1++;
+            }else if(nums[i] == m2){
+                cnt2++;
+            }else if(cnt1 == 0){
+                m1 = nums[i];
+                cnt1++;
+            }else if(cnt2 == 0){
+                m2 = nums[i];
+                cnt2++;
+            }else{
+                cnt1--, cnt2--;
+            }
+            
+        }
+        
+        cout << m1 << " " << m2 << endl;
+        
         vector<int> res;
-        for(auto x : nums){
-            mp[x]++;
+        cnt1 = 0, cnt2 = 0;
+        for(int i = 0; i < n; i++){
+            if(nums[i] == m1) cnt1++;
+            else if(nums[i] == m2) cnt2++;
         }
         
-        for(auto x : mp){
-            if(x.second > mr) 
-                res.push_back(x.first);
-        }
-        
+        if(cnt1 > n / 3) res.push_back(m1);
+        if(cnt2 > n / 3) res.push_back(m2);
         
         return res;
     }
