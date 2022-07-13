454c62f7608fd4a7cf67b96eba661351c1fe01f0
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-07-13 13:24:22 +0530

    Time: 16 ms (65.87%), Space: 10.8 MB (5.14%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -17,23 +17,20 @@ public:
         ListNode* node = head;
         ListNode* tail = head;
         
-        int count = 0, evens = 0;
+        double count = 0;
         
         while(tail->next != NULL){
             count += 1;
             tail = tail->next;
         }
         
-        while(evens < count / 2 + ((count + 1) % 2 == 0)){
+        for(int i = 0; i < ceil(count / 2); i++){
             
             tail->next = new ListNode(node->next->val);
             tail = tail->next;
-            evens++;
             
             node->next = node->next->next;
             node = node->next;
-            // cout << evens << " " << count / 2 << endl;
-            // cout << tail->val << " " << node->val << " " << first << endl;
             
         }
         
