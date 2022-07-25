14f90773e760bbf237dd365af062b55601f262c4
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-07-25 07:03:00 +0530

    Time: 15 ms (39.90%), Space: 13.6 MB (63.15%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -4,31 +4,26 @@ public:
         
         if(nums.size() == 0) return {-1, -1};
         
-        int l = 0, r = nums.size() - 1, first = -1, last = -1;
+        int l = 0, r = nums.size() - 1, first = -1, last = -1, l1 = 0, r1 = nums.size() - 1;
         
-        while(l <= r){
-            int md = (l + r) / 2;
+        while(l <= r || l1 <= r1){
+            int md1 = (l + r) / 2, md2 = (l1 + r1) / 2;
             
-            if(nums[md] == target){
-                first = md;
-                r = md - 1;
-            }else if(nums[md] > target) r = md - 1;
-            else l = md + 1;
+            if(l <= r){
+                if(nums[md1] == target){
+                    first = md1;
+                    r = md1 - 1;
+                }else if(nums[md1] > target) r = md1 - 1;
+                else l = md1 + 1;    
+                
+            }else{
+                if(nums[md2] == target){
+                    last = md2;
+                    l1 = md2 + 1;
+                }else if(nums[md2] > target) r1 = md2 - 1;
+                else l1 = md2 + 1;    
+            }
             
-        }
-        
-        if(first == -1) return {-1, -1};
-        
-        l = first, r = nums.size() - 1;
-        
-        while(l <= r){
-            int md = (l + r) / 2;
-            
-            if(nums[md] == target){
-                last = md;
-                l = md + 1;
-            }else if(nums[md] > target) r = md - 1;
-            else l = md + 1;
             
         }
         
