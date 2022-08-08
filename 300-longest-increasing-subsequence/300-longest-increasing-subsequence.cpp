7f225ae5813fcdfcea0b3c2a491748c4b62af42c
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-08-08 08:58:19 +0530

    Time: 426 ms (47.18%), Space: 10.6 MB (47.88%) - LeetHub

---
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> dp(n, 1);
+        
+        for(int i = 1; i < nums.size(); i++){
+            for(int j = 0; j < i; j++){
+                if(nums[j] < nums[i] and dp[i] < dp[j] + 1){
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+        
+        return *max_element(dp.begin(), dp.end());
+    }
+};
