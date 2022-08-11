927e33707acbcb3c2583d2c96f6e81611ca9e84d
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-08-11 07:38:13 +0530

    Time: 14 ms (80.78%), Space: 21.7 MB (30.71%) - LeetHub

---
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    bool check(TreeNode* root, long long parent, string subtree){
+        if(root == NULL) return true;
+        
+        if(subtree == "left" and root->val >= parent) return false;
+        if(subtree == "right" and root->val <= parent) return false;
+        
+        bool left = check(root->left, root->val, "left");
+        
+        bool right = check(root->right, root->val, "right");
+        
+        return left and right;
+    }
+    
+    vector<int> sorted;
+    
+    bool dfs(TreeNode* root){
+        if(root == NULL) return true;
+        
+        bool left = dfs(root->left);
+
+        if(sorted.size() > 0 and sorted.back() >= root->val) return false;
+        sorted.push_back(root->val);
+        
+        bool right = dfs(root->right);
+        
+        return left and right;
+    }
+    
+    
+    bool isValidBST(TreeNode* root) {
+        // return check(root, LONG_MAX, "");
+        return dfs(root);
+    }
+};
