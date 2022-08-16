3d13f382bd591954f3f14b7fddf67f4fe66b4bb7
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-08-16 06:16:05 +0530

    Time: 22 ms (94.75%), Space: 10.7 MB (43.91%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,20 +1,15 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        map<char, pair<int, int>> dic;
-        for(int i = 0; i < s.length(); i++){
-            auto prev = dic[s[i]];
-            dic[s[i]] = {prev.first + 1, i};
+        vector<int> hash(26, 0);
+        for(auto &x : s){
+            hash[x - 'a']++;
         }
         
-        int res = INT_MAX;
-        for(auto x : dic){
-            auto info = x.second;
-            if(info.first == 1) {
-                res = min(res, info.second);
-            }
+        for(int i = 0; i < s.length(); i++){
+            if(hash[s[i] - 'a'] == 1) return i;
         }
-            
-        return res == INT_MAX ? -1 : res;
+        
+        return -1;
     }
 };
