867b30d0017a7d80b8aa435f504337d8b3ba1e6e
Gokul Varadan <51291254+gokulcodes@users.noreply.github.com>
2022-09-01 09:51:58 +0530

    Time: 319 ms (5.02%), Space: 86.4 MB (9.98%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -12,69 +12,25 @@
 class Solution {
 public:
     
-    // int dfs(TreeNode* root, vector<int>& hash){
-        // if(root == NULL) return 0;
-        
-//         if(root->left == NULL and root->right == NULL) {
-//             hash.push_back(root->val);
-//             for(auto x : hash) cout << x << " ";
-//             cout << endl;
-//             return 0;
-//         }
-        
-//         hash.push_back(root->val);
-        
-//         if(root->left != NULL){
-//             dfs(root->left, hash);
-//             hash.pop_back();
-//         }
-        
-//         for(auto x : hash) cout << x << " ";
-//         cout << endl;
-        
-//         if(root->right != NULL){
-//             dfs(root->right, hash);
-//             hash.pop_back();        
-//         }
-        
-//         return 0;
-//     }
-    
     int cnt = 1;
     void dfs(TreeNode* &root){
-        if(root == NULL) return;
         
         if(root->left != NULL){
-            if(root->val <= root->left->val) {
-                // cout << root->val << " " << root->left->val << endl;
-                cnt++;
-            }
+            if(root->val <= root->left->val) cnt++;
             root->left->val = max(root->val, root->left->val);
             dfs(root->left);    
         }
         
         if(root->right != NULL){
-            if(root->val <= root->right->val) {
-                // cout << root->val << " " << root->right->val << endl;
-                cnt++;
-            }
+            if(root->val <= root->right->val) cnt++;
             root->right->val = max(root->val, root->right->val);
             dfs(root->right);    
         }
         
     }
     
-    // void traverse(TreeNode* root){
-    //     if(root==NULL) return;
-    //     traverse(root->left);
-    //     cout << root->val << endl;
-    //     traverse(root->right);
-    // }
-    
     int goodNodes(TreeNode* root) {
-        // vector<int> hash;
         dfs(root);
-        // traverse(root);
         return cnt;
     }
 };
